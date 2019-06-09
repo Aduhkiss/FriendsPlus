769608b32b178f008ccd4096e940a96abeaa3149
Atticus Zambrana <angusbeefsteak14@gmail.com>
2019-06-09 16:22:06 -0700

    Added a message when the player leaves the server

--- src/me/atticusthecoder/friendsplus/handler/PlayerQuit.java
+++ src/me/atticusthecoder/friendsplus/handler/PlayerQuit.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerQuitEvent;
@@ -12,6 +13,7 @@ import me.atticusthecoder.friendsplus.api.PlayerManager;
 import me.atticusthecoder.friendsplus.data.DataHandler;
 import me.atticusthecoder.friendsplus.object.Friend;
 import me.atticusthecoder.friendsplus.object.PlayerData;
+import net.md_5.bungee.api.ChatColor;
 
 public class PlayerQuit implements Listener {
 	
@@ -25,11 +27,20 @@ public class PlayerQuit implements Listener {
 			friendsStr.add(f.getName());
 		}
 		
+		// Let this persons friends know they left the server
+		for(Friend f : data.getFriends()) {
+			if(f.isOnline()) {
+				Player friend = Bukkit.getPlayer(f.getName());
+				friend.sendMessage(ChatColor.AQUA + "Your friend " + e.getPlayer().getName() + " has left.");
+			}
+		}
+		
+		
 		boolean requests = PlayerManager.get().getPlayerData(e.getPlayer().getName()).getRequestsEnabled();
 		DataHandler.getInstance().getFriendData().set("players." + e.getPlayer().getUniqueId() + ".requests", requests);
 		
 		DataHandler.getInstance().getFriendData().set("players." + e.getPlayer().getUniqueId() + ".friends", friendsStr);
 		Bukkit.getLogger().info("[Friends+] Saved friend data for " + e.getPlayer().getName() + ".");
-	}
+		}
 
 }
